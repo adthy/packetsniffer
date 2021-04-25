e293ea9a47f2e9519ec0efe961e8af1e7f01f4bd
Akshay <akshay.cs18@bmsce.ac.in>
2021-04-25 15:54:49 +0530

    Formatting

--- sniffer.py
+++ sniffer.py
@@ -20,6 +20,8 @@ DATA_TAB_3 = '\t\t\t   '
 DATA_TAB_4 = '\t\t\t\t   '
 
 # Returns MAC as string from bytes (ie AA:BB:CC:DD:EE:FF)
+
+
 def get_mac_addr(mac_raw):
     byte_str = map('{:02x}'.format, mac_raw)
     mac_addr = ':'.join(byte_str).upper()
@@ -35,6 +37,7 @@ def format_multi_line(prefix, string, size=80):
             size -= 1
     return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
 
+
 def main():
     pcap = Pcap('capture.pcap')
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
@@ -45,20 +48,24 @@ def main():
         eth = Ethernet(raw_data)
 
         print('\nEthernet Frame:')
-        print(TAB_1 + 'Destination: {}, Source: {}, Protocol: {}'.format(eth.dest_mac, eth.src_mac, eth.proto))
+        print(TAB_1 + 'Destination: {}, Source: {}, Protocol: {}'.format(eth.dest_mac,
+                                                                         eth.src_mac, eth.proto))
 
         # IPv4
         if eth.proto == 8:
             ipv4 = IPv4(eth.data)
             print(TAB_1 + 'IPv4 Packet:')
-            print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {},'.format(ipv4.version, ipv4.header_length, ipv4.ttl))
-            print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4.proto, ipv4.src, ipv4.target))
+            print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {},'.format(
+                ipv4.version, ipv4.header_length, ipv4.ttl))
+            print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4.proto,
+                                                                        ipv4.src, ipv4.target))
 
             # ICMP
             if ipv4.proto == 1:
                 icmp = ICMP(ipv4.data)
                 print(TAB_1 + 'ICMP Packet:')
-                print(TAB_2 + 'Type: {}, Code: {}, Checksum: {},'.format(icmp.type, icmp.code, icmp.checksum))
+                print(TAB_2 + 'Type: {}, Code: {}, Checksum: {},'.format(icmp.type,
+                                                                         icmp.code, icmp.checksum))
                 print(TAB_2 + 'ICMP Data:')
                 print(format_multi_line(DATA_TAB_3, icmp.data))
 
@@ -66,11 +73,15 @@ def main():
             elif ipv4.proto == 6:
                 tcp = TCP(ipv4.data)
                 print(TAB_1 + 'TCP Segment:')
-                print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(tcp.src_port, tcp.dest_port))
-                print(TAB_2 + 'Sequence: {}, Acknowledgment: {}'.format(tcp.sequence, tcp.acknowledgment))
+                print(
+                    TAB_2 + 'Source Port: {}, Destination Port: {}'.format(tcp.src_port, tcp.dest_port))
+                print(
+                    TAB_2 + 'Sequence: {}, Acknowledgment: {}'.format(tcp.sequence, tcp.acknowledgment))
                 print(TAB_2 + 'Flags:')
-                print(TAB_3 + 'URG: {}, ACK: {}, PSH: {}'.format(tcp.flag_urg, tcp.flag_ack, tcp.flag_psh))
-                print(TAB_3 + 'RST: {}, SYN: {}, FIN:{}'.format(tcp.flag_rst, tcp.flag_syn, tcp.flag_fin))
+                print(TAB_3 + 'URG: {}, ACK: {}, PSH: {}'.format(tcp.flag_urg,
+                                                                 tcp.flag_ack, tcp.flag_psh))
+                print(TAB_3 + 'RST: {}, SYN: {}, FIN:{}'.format(tcp.flag_rst,
+                                                                tcp.flag_syn, tcp.flag_fin))
 
                 if len(tcp.data) > 0:
 
@@ -86,14 +97,15 @@ def main():
                             print(format_multi_line(DATA_TAB_3, tcp.data))
                     else:
                         print(TAB_2 + 'TCP Data:')
-            
+
                         print(format_multi_line(DATA_TAB_3, tcp.data))
 
             # UDP
             elif ipv4.proto == 17:
                 udp = UDP(ipv4.data)
                 print(TAB_1 + 'UDP Segment:')
-                print(TAB_2 + 'Source Port: {}, Destination Port: {}, Length: {}'.format(udp.src_port, udp.dest_port, udp.size))
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}, Length: {}'.format(
+                    udp.src_port, udp.dest_port, udp.size))
 
             # Other IPv4
             else:
